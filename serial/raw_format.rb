17521c5b2b5e5a356a54b134ce860386649f85a0
ikesato <ikesato76@gmail.com>
2012-12-05 03:42:15 +0900

    implement raw format

--- serial/raw_format.rb
+++ serial/raw_format.rb
@@ -3,6 +3,8 @@
 # 生データを表示するだけ
 # 単位は ms
 class RawFormat
+  NAME = "raw"
+
   def initialize(raw)
     @raw = raw
   end
@@ -11,6 +13,12 @@ class RawFormat
   end
 
   def dump
-    "raw [#{@raw.map{|v| sprintf("%.3f",v)}.join(",")}]"
+    "#{NAME} [#{@raw.map{|v| sprintf("%.3f",v)}.join(",")}]"
+  end
+
+  def self.make_send_ary(str)
+    return false if str !~ /#{NAME} \[([\da-f\.,]+)\]/
+    raw = $1.split(",").map{|v| v.to_f}
+    raw
   end
 end
